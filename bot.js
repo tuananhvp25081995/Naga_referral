a2a545b1d96c6f6a5b6a18d255d6970bfe8448ef
Tuan Anh <tuananhvp25895@gmail.com>
2022-08-16 12:23:20 +0700

    update

--- bot.js
+++ bot.js
@@ -259,7 +259,7 @@ bot.on("message", async (...parameters) => {
                     } catch (e) {
                     }
                     if (checkIsVoted.href.indexOf("https://nagakingdom.com") != -1 || checkIsVoted.href.indexOf("https://naga.gg") != -1) {
-                        if (checkIsVoted.query != undefined) {
+                        if (checkIsVoted.query.refCode != undefined) {
                             await UserModel.updateOne({ telegramID }, { "registerFollow.step4.refCode": checkIsVoted.query.refCode ,"registerFollow.step4.isWaitingPass": true}).exec();
                             return sendStep6_Finish({telegramID},bot)
                         }
